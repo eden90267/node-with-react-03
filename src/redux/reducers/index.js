34e4a2787032193ea48f929534ebfbb02bd3172c
eden90267 <eden90267@gmail.com>
2017-08-22 07:59:12 +0800

    Impl

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,8 +4,10 @@ import {routerReducer} from 'react-router-redux';
 import {todos} from './todoReducer';
 import {userInfo} from "./userInfo";
 import {article} from "./article";
+import {waiting} from "./waiting";
 
 const rootReducer = combineReducers({
+  waiting,
   todos,
   userInfo,
   article,
